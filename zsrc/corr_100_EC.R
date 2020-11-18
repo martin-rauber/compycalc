ffaa98b0c5b08c7f452a1bedb6f1aa656383fd2f
Martin Rauber <martin.rauber@gmail.com>
2020-11-18 09:26:13 +0100

    v1.1.5

--- zsrc/corr_100_EC.R
+++ zsrc/corr_100_EC.R
@@ -7,8 +7,8 @@
   library(MASS)
 SS_for_a<-function(par) {
   a<-par[1:n_taken];b<-b_fixed;T<-par[(n_taken+1):length(par)]
-  alfa.nv<-exp(log(alfa0)*exp(5-(1/T))) # non volatile
-  alfa.v<-exp(log(alfa0)*exp(5-(b/T)))  # volatile
+  alfa.nv<-exp(log(alfa0)*exp(Ea_RT0-(1/T))) # non volatile
+  alfa.v<-exp(log(alfa0)*exp(Ea_RT0-(b/T)))  # volatile
   Ys<-(alfa.nv+(a*alfa.v))/(1+a)
   Fs<-((a*alfa.v*Fv)+(alfa.nv*Fnv))/(Ys*(1+a))
   sum_sqrs<-1/((F_meas_sig^2+Y_meas_sig^2))*sum((Fs-Fm_taken)^2+(Ys-Y_taken)^2)
@@ -17,15 +17,15 @@ SS_for_a<-function(par) {
 
 alfas<-function(par) {
   a<-par[1];b<-par[2];T<-par[3:length(par)]
-  alfa.nv<-exp(log(alfa0)*exp(5-(1/T))) # non volatile
-  alfa.v<-exp(log(alfa0)*exp(5-(b/T)))  # volatile
+  alfa.nv<-exp(log(alfa0)*exp(Ea_RT0-(1/T))) # non volatile
+  alfa.v<-exp(log(alfa0)*exp(Ea_RT0-(b/T)))  # volatile
   return(c(alfa.v))
 }
 
 SS_forT<-function(par) {
   a<-par[1];b<-par[2];T<-par[3:length(par)]
-  alfa.nv<-exp(log(alfa0)*exp(5-(1/T))) # non volatile
-  alfa.v<-exp(log(alfa0)*exp(5-(b/T)))  # volatile
+  alfa.nv<-exp(log(alfa0)*exp(Ea_RT0-(1/T))) # non volatile
+  alfa.v<-exp(log(alfa0)*exp(Ea_RT0-(b/T)))  # volatile
   Ys<-(alfa.nv+(a*alfa.v))/(1+a)
   Fs<-((a*alfa.v*Fv)+(alfa.nv*Fnv))/(Ys*(1+a))
   sum_sqrs<-sum((Fs-F_meas)^2+(Ys-Y_meas)^2)
@@ -34,8 +34,8 @@ SS_forT<-function(par) {
 
 Fs_forT<-function(par) {
   a<-par[1];b<-par[2];T<-par[3:length(par)]
-  alfa.nv<-exp(log(alfa0)*exp(5-(1/T))) # non volatile
-  alfa.v<-exp(log(alfa0)*exp(5-(b/T)))  # volatile
+  alfa.nv<-exp(log(alfa0)*exp(Ea_RT0-(1/T))) # non volatile
+  alfa.v<-exp(log(alfa0)*exp(Ea_RT0-(b/T)))  # volatile
   Ys<-(alfa.nv+(a*alfa.v))/(1+a)
   Fs<-((a*alfa.v*Fv)+(alfa.nv*Fnv))/(Ys*(1+a))
   return(c(Ys,Fs))
@@ -69,6 +69,7 @@ F_meas = F14C_EC_raw_data
 #####
 
 b_fixed<-0.86 #----> b IS FIXED <-----------
+Ea_RT0<-7.5   # ----> max Ea/RT0 value <---
 a<-c(); T<-c(); alfa.nv.all<-c(); alfa.v.all<-c(); Ys.all<-c(); Fs.all<-c(); linmodel_slope<-c(); F0.all<-c(); a_sig<-c(); T_sig<-c()
 n<-length(Y_meas); Fnv<-0; Fv<-1.11  #main parameter values
 Fnv_sig<-0.03; Fv_sig<-0.03*Fv; F_meas_sig<-0.03;Y_meas_sig<-0.01 #main parameters precision for error propagation
@@ -77,7 +78,7 @@ Fnv_sig<-0.03; Fv_sig<-0.03*Fv; F_meas_sig<-0.03;Y_meas_sig<-0.01 #main paramete
 alfa0<-0.045; T0<-0.10
 initial_par<-c(rep(1,n),rep(T0,n))          #vector of initial a's and T's
 low_par<-c(rep(0.01,n),rep(0.1*T0,n))       #vector of lower bound a's and T's
-up_par<-c(rep(5,n),rep(0.25,n))             #vector of upper bound a's and T's
+up_par<-c(rep(Ea_RT0,n),rep(0.25,n))             #vector of upper bound a's and T's
 n_taken<-n; Fm_taken<-F_meas; Y_taken<-Y_meas
 model3<-optim(initial_par,SS_for_a,lower=low_par,upper=up_par,method="L-BFGS-B",hessian=TRUE) #error MINIMIZATION changing a & T on non linear model
 a<-model3$par[1:n]; T<-model3$par[(n+1):(2*n)]
@@ -90,7 +91,7 @@ for (samples in c(1:n)) {
   alfa0<-0.045; T0<-0.10;n_taken<-1;H<-c()
   initial_par<-c(rep(1,n_taken),rep(T0,n_taken))          #vector of initial a's and T's
   low_par<-c(rep(0.01,n_taken),rep(0.1*T0,n_taken))       #vector of lower bound a's and T's
-  up_par<-c(rep(5,n_taken),rep(0.25,n_taken))             #vector of upper bound a's and T's
+  up_par<-c(rep(Ea_RT0,n_taken),rep(0.25,n_taken))             #vector of upper bound a's and T's
   Fm_taken<-F_meas[samples]; Y_taken<-Y_meas[samples]
   model3<-optim(initial_par,SS_for_a,lower=low_par,upper=up_par,method="L-BFGS-B",hessian=TRUE) #error MINIMIZATION changing a & T on non linear model
   a[samples]<-model3$par[1]; T[samples]<-model3$par[2]
@@ -99,8 +100,8 @@ for (samples in c(1:n)) {
   a_sig[samples]<-sqrt(abs(variance[1])); T_sig[samples]<-sqrt(abs(variance[2]))
 }
 
-alfa.nv.all<-alfa0^(exp(5-(1/T))) # non volatile
-alfa.v.all<-alfa0^(exp(5-(b_fixed/T)))  # volatile
+alfa.nv.all<-alfa0^(exp(Ea_RT0-(1/T))) # non volatile
+alfa.v.all<-alfa0^(exp(Ea_RT0-(b_fixed/T)))  # volatile
 Ys.all<-(alfa.nv.all+(a*alfa.v.all))/(1+a) #calculated yield
 Fs.all<-((a*alfa.v.all*Fv)+(alfa.nv.all*Fnv))/(Ys.all*(1+a)) #calculated Fm
 linmodel_slope<-((a*Fv/(1+a))-F_meas)/(1-Y_meas)  #slope of linear model
@@ -111,8 +112,8 @@ F0.all<-((a*Fv)+Fnv)/(1+a)  #Fm correction by extrapolation to yield=1 non linea
 
 F0_err_a<-(Fv/(1+a))-(Fnv/((1+a)^2))-(a*Fv/((1+a)^2))
 F0_err_Fv<-a/(1+a); F0_err_Fnv<-1/(1+a)
-alfav_err_T<-b_fixed*log(alfa0)*exp(5-(b_fixed/T))*(1/T^2)*alfa0^(exp(5-(b_fixed/T)))
-alfanv_err_T<-log(alfa0)*exp(5-(1/T))*(1/T^2)*alfa0^(exp(5-(1/T)))
+alfav_err_T<-b_fixed*log(alfa0)*exp(Ea_RT0-(b_fixed/T))*(1/T^2)*alfa0^(exp(Ea_RT0-(b_fixed/T)))
+alfanv_err_T<-log(alfa0)*exp(Ea_RT0-(1/T))*(1/T^2)*alfa0^(exp(Ea_RT0-(1/T)))
 F0_err_alfav<-(a*Fv/(1+a))-(((a^2*Fv)+(a*Fnv))/((1+a)^2))
 F0_err_alfanv<-(Fnv/(1+a))-(((a*Fv)+Fnv)/((1+a)^2))
 F0_sig_total<-sqrt((F0_err_a*a_sig)^2 +(F0_err_Fv*Fv_sig)^2 + (F0_err_Fnv*Fnv_sig)^2 + (F0_err_alfav*alfav_err_T*T_sig)^2 + (F0_err_alfanv*alfanv_err_T*T_sig)^2)
